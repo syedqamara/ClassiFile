008e5ed96761992dbb77885efe8541d54f6797c8
Syed Qamar Abbas <syedqamar.a1@gmail.com>
2018-03-25 22:57:25 +0500

    Initial Commit

---
+++ ClassiFile/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ClassiFile
+//
+//  Created by Syed Qamar Abbas on 25/03/2018.
+//  Copyright © 2018 QamarAbbas. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ClassiFile/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ClassiFile
+//
+//  Created by Syed Qamar Abbas on 25/03/2018.
+//  Copyright © 2018 QamarAbbas. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
